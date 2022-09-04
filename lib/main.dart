d7872d196d9a3dc7e529931982f057a915414cc5
githubMyoThant <myothantdev@gmail.com>
2022-09-04 09:32:38 -0700

    To Learn Myanmar Number 0.0.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: avoid_print, non_constant_identifier_names
 
+import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
@@ -35,9 +37,31 @@ class _MyHomePageState extends State<MyHomePage> {
   var secondImage = 4;
   var thirdImage = 6;
   var fourthImage = 9;
+  int scores = 0;
+  int result_score = 0;
 
   List myanmarNumList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   var showAnswer = '';
+  SharedPreferences? pref;
+
+  @override
+  void initState() {
+    super.initState();
+    getIntValuesSF();
+  }
+
+  storeIntToSF(int newScore) async {
+    pref = await SharedPreferences.getInstance();
+    pref?.setInt("result", newScore);
+  }
+
+  getIntValuesSF() async {
+    pref = await SharedPreferences.getInstance();
+    var myscore = pref!.getInt("result")!;
+    setState(() {
+      result_score = myscore;
+    });
+  }
 
   void getRandom() {
     myanmarNumList.shuffle();
@@ -58,8 +82,11 @@ class _MyHomePageState extends State<MyHomePage> {
   void checkAnswer(int cardNumber) {
     setState(() {
       if (engNumber == cardNumber) {
+        scores = scores + 10;
+        result_score = result_score + 10;
         scaffoldMsgSuccess();
         visible = 1;
+        storeIntToSF(result_score);
       } else {
         scaffoldMsgFailed();
       }
@@ -92,9 +119,9 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void scaffoldMsgSuccess() {
     setState(() {
-      const snackBar = SnackBar(
-        content: Text('Your answer is Correct'),
-        duration: Duration(milliseconds: 1500),
+      var snackBar = SnackBar(
+        content: Text('Your answer is Correct and scores is : $scores'),
+        duration: const Duration(milliseconds: 1500),
         backgroundColor: Colors.green,
       );
       ScaffoldMessenger.of(context).showSnackBar(snackBar);
@@ -140,6 +167,9 @@ class _MyHomePageState extends State<MyHomePage> {
                 fontWeight: FontWeight.w600,
               ),
             ),
+            Opacity(
+                opacity: visible,
+                child: Text('Your Previous Score is $result_score')),
             const SizedBox(
               height: 40,
             ),
@@ -166,7 +196,7 @@ class _MyHomePageState extends State<MyHomePage> {
             Opacity(
               opacity: visible,
               child: nextLesson(),
-            )
+            ),
           ],
         ),
       ),
